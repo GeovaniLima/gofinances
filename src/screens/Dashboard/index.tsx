d08089bc0340df547e8349529ece5b2637b22021
GeovaniLima <geovani.lima.silva@hotmail.com>
2022-02-10 15:12:20 -0300

    Finalizando a listagem

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { HighlightCard } from '../../components/HighlightCard';
-import { TransactionCard } from '../../components/TransactionCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -20,34 +19,44 @@ import {
   TransactionList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
-  const data = [
+  const data: DataListProps[] = [
     {
+      id: '1',
+      type: 'positive',
       title: 'Desenvolvimento de Sites',
       amount:'R$ 12.000,00',
       category: {
         name: 'Vendas',
-        icon: 'dollar-sing'
+        icon: 'dollar-sign'
       },
       date:'13/04/2020'
     },
     {
-      title: 'Desenvolvimento de Sites',
-      amount:'R$ 12.000,00',
+      id: '2',
+      type: 'negative',
+      title: 'Hamburgueria Pizzy',
+      amount:'R$ 59,00',
       category: {
-        name: 'Vendas',
-        icon: 'dollar-sing'
+        name: 'Alimentação',
+        icon: 'coffee'
       },
-      date:'13/04/2020'
+      date:'10/04/2020'
     },
     {
-      title: 'Desenvolvimento de Sites',
-      amount:'R$ 12.000,00',
+      id: '3',
+      type: 'negative',
+      title: 'Aluguel do apartamento',
+      amount:'R$ 1.200,00',
       category: {
-        name: 'Vendas',
-        icon: 'dollar-sing'
+        name: 'Casa',
+        icon: 'shopping-bag'
       },
-      date:'13/04/2020'
+      date:'10/04/2020'
     }
   ];
 
@@ -96,11 +105,8 @@ export function Dashboard() {
         
         <TransactionList 
           data={data}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => <TransactionCard data={item}/>}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: getBottomSpace()
-          }}
         />
         
       </Transactions>
